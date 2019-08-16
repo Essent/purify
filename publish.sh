582e204537222aa47ab477736b2bc86a94277e69
Elmer van Rooijen <elmer.van.rooijen@essent.nl>
2019-08-16 10:28:43 +0200

    add shell publish script

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+declare -a files=("package.json" "LICENSE" "README.md" "yarn.lock")
+
+npm run build
+
+for i in "${files[@]}"
+do
+    cp $i lib/$i
+done
+
+cd lib
+ls
+npm publish --dry-run
